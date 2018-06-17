dbaf990887835092cc78426ab9ab20f6b9b73cd9
Daniel Chung <danetix@gmail.com>
2018-06-17 03:08:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,8 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.size > 1
-    puts "The line is empty."
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
   else
     puts "There is nobody waiting to be served!"
   end
